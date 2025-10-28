33c9e37f7a523745dc2a947b92a14554da8b8055
Shadin mc <Shadinmc2002@gmail.com>
2025-10-28 17:07:05 +0530

    Completed Cod Payment

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,8 +1,11 @@
 const db = require('../config/connection');
 const bcrypt = require('bcrypt');
 const collection = require('../config/collections');
-const { response, use } = require('../app');
+const { response, use, resource } = require('../app');
 var objectid = require('mongodb').ObjectId
+const Razorpay = require('razorpay')
+var instance = new Razorpay({ key_id: 'rzp_test_RYqHM2J0hEBZCJ', key_secret: 'vfbc34tn8oPdtgCEfV0A2pED' })
+
 
 module.exports = {
   doSignup: (userData) => {
@@ -132,11 +135,11 @@ module.exports = {
 
     return new Promise((resolve, reject) => {
       console.log(details)
-      if (details.count==-1 && details.quantity==1) {
+      if (details.count == -1 && details.quantity == 1) {
         db.get().collection(collection.CART_COLLECTION).updateOne(
           { _id: new objectid(details.cart) },
           { $pull: { products: { item: new objectid(details.product) } } }
-          
+
         ).then(() => {
           resolve({ removeProduct: true })
         })
@@ -165,7 +168,7 @@ module.exports = {
       let total = await db.get().collection(collection.CART_COLLECTION).aggregate([
         { $match: { user: new objectid(userId) } },
         { $unwind: '$products' },
-         {
+        {
           $project: {
             item: '$products.item',
             quantity: '$products.quantity',
@@ -189,15 +192,85 @@ module.exports = {
         {
           $group: {
             _id: null,
-           total:{$sum:{$multiply:['$quantity',{ $toDouble: '$product.Price' }]}}
+            total: { $sum: { $multiply: ['$quantity', { $toDouble: '$product.Price' }] } }
           }
         }
       ]).toArray()
-      resolve(total[0].total)
+      if (total.length > 0) {
+        resolve(total[0].total)
+      } else {
+        resolve(0)
+      }
     })
+  },
+  placeOrder: (orderData, products, total) => {
+    return new Promise((resolve, reject) => {
+      let status = orderData['paymentMethod'] === 'cod' ? 'placed' : 'pending'
+      let orderObj = {
+        deliveryDetails: {
+          address: orderData.address,
+          phone: orderData.phone,
+          pincode: orderData.pincode
+        },
+        userId: new objectid(orderData.userId),
+        paymentMethod: orderData.paymentMethod,
+        products: products,
+        totalAmount: total,
+        status: status,
+        date: new Date()
+      }
+
+      db.get().collection(collection.ORDER_COLLECTION)
+        .insertOne(orderObj)
+        .then((response) => {
+          resolve(response.insertedId)
+        })
+    })
+  },
+  deleteCart: (userId) => {
+    return new Promise((resolve, reject) => {
+      db.get().collection(collection.CART_COLLECTION)
+        .deleteOne({ user: new objectid(userId) })
+        .then(() => resolve())
+    })
+  }, getUserOrders: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      let orders = await db.get().collection(collection.ORDER_COLLECTION)
+        .find({ userId: new objectid(userId) })
+        .sort({ _id: -1 })
+        .toArray()
+      resolve(orders)
+    })
+  },
+  getOrderDetails: (orderId) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let order = await db.get().collection(collection.ORDER_COLLECTION)
+          .findOne({ _id: new objectid(orderId) });
+        resolve(order);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  },
+  generateRazorPay: (orderId,total) => {
+    return new  Promise((resolve,reject)=>{
+    var options = {
+      amount: total,  // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      receipt: orderId
+    };
+    instance.orders.create(options, function (err, order) {
+      console.log("New order :",order);
+      resolve(order)
+    });
+  })
   }
+
+
+
 }
- 
+
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
 var userHelpers = require('../helpers/user-helpers');
 const { response } = require('../app');
+
 const verifyLogin = (req, res, next) => {
   if (req.session.loggedIn) {
     next()
@@ -11,15 +12,15 @@ const verifyLogin = (req, res, next) => {
   }
 }
 /* GET home page. */
-router.get('/',async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   let user = req.session.user
-  let cartCount=null
-  if(req.session.user){
-  cartCount =await userHelpers.getCartCount(req.session.user._id)
-  console.log(cartCount)
+  let cartCount = null
+  if (req.session.user) {
+    cartCount = await userHelpers.getCartCount(req.session.user._id)
+    console.log(cartCount)
   }
   productHelpers.getAllProducts().then((products) => {
-    res.render('user/view-products', { products, user,cartCount })
+    res.render('user/view-products', { products, user, cartCount })
   })
 })
 router.get('/login', (req, res) => {
@@ -36,8 +37,8 @@ router.get('/login', (req, res) => {
 router.post('/signup', (req, res) => {
   userHelpers.doSignup(req.body).then((response) => {
     console.log("User signed up successfully:", response);
-    req.session.loggedIn=true
-    req.session.user=response
+    req.session.loggedIn = true
+    req.session.user = response
     res.redirect('/');
   }).catch((err) => {
     console.error("Signup failed:", err);
@@ -61,19 +62,19 @@ router.get('/logout', (req, res) => {
   res.redirect('/')
 
 })
-router.get('/cart',verifyLogin,async (req, res) => {
-  let products=await userHelpers.getCartProducts(req.session.user._id)
+router.get('/cart', verifyLogin, async (req, res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id)
   products.forEach(p => {
     p.totalPrice = parseInt(p.product.Price) * parseInt(p.quantity);
-      console.log(p.totalPrice)
+    console.log(p.totalPrice)
 
   });
-  res.render('user/cart',{products,user:req.session.user})
+  res.render('user/cart', { products, user: req.session.user })
 })
-router.get('/add-to-cart/:id',(req,res)=>{
+router.get('/add-to-cart/:id', (req, res) => {
   console.log('api call')
-  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
-    res.json({status:true})
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.json({ status: true })
   })
 })
 
@@ -84,15 +85,55 @@ router.post('/remove-product', (req, res) => {
 });
 
 
-router.post('/change-product-quantity',(req,res,next)=>{
-  userHelpers.changeProductQuantity(req.body).then((response)=>{
-      console.log(response)
-    res.json({response})
+router.post('/change-product-quantity', (req, res, next) => {
+  userHelpers.changeProductQuantity(req.body).then((response) => {
+    console.log(response)
+    res.json({ response })
   })
 })
-router.get('/place-order',verifyLogin,async(req,res)=>{
-  let total=await userHelpers.getTotalAmount(req.session.user._id)
-  res.render('user/place-order',{total})
+router.get('/place-order', verifyLogin, async (req, res) => {
+  let total = await userHelpers.getTotalAmount(req.session.user._id)
+  res.render('user/place-order', { total })
 })
+router.post('/place-order', verifyLogin, async (req, res) => {
+    const userId = req.session.user._id;
+    const products = await userHelpers.getCartProducts(userId);
+    const total = await userHelpers.getTotalAmount(userId);
+
+    // Create the order once
+    const orderId = await userHelpers.placeOrder({ ...req.body, userId }, products, total);
+
+    if (req.body['paymentMethod'] === 'cod') {
+      await userHelpers.deleteCart(userId);
+      return res.redirect('/order-success');
+    } else {
+      userHelpers.generateRazorPay(orderId, total).then((response)=>{
+        res.json(response)
+      })
+      
+        
+    }
+  } );
+
+router.get('/order-success', (req, res) => {
+  res.render('user/order-success', { user: req.session.user })
+})
+router.get('/orders', verifyLogin, async (req, res) => {
+  let userId = req.session.user._id
+  let orders = await userHelpers.getUserOrders(userId)
+  console.log('Fetched Orders:', orders);
+  res.render('user/orders', { user: req.session.user._id, orders })
+})
+router.get('/order-details/:id', verifyLogin, async (req, res) => {
+  try {
+    let orderId = req.params.id;
+    let order = await userHelpers.getOrderDetails(orderId);
+    res.render('user/order-details', { user: req.session.user._id, order });
+  } catch (err) {
+    console.log('Error fetching order details:', err);
+    res.status(500).send('Error loading order details');
+  }
+});
+
 
 module.exports = router;
